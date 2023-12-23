31c6720db75e6540319457f24d1e7fc3d0beeef4
froehlicha <froehlich.alexander@protonmail.com>
2023-12-23 23:24:23 +0100

    add index page

--- options.mjs
+++ options.mjs
@@ -2,12 +2,22 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 import fsx from 'node:fs';
 
+const index = await fs.readFile(path.join('src', 'index.html'), 'utf8');
 const template = await fs.readFile(path.join('src', 'template.html'), 'utf8');
 const files = await fs.readdir(path.join('src', 'pages'));
 const scripts = files.filter(f => f.endsWith('.js'));
 
 await fs.rm('public', { recursive: true });
 await fs.mkdir('public');
+
+// Generate Index
+let output = index.replace(
+    '<ul class="links"></ul>',
+    `<ul class="links">\n${scripts.map(
+        s => `    <li><a href="${s.replace(".js", ".html")}">${s.replace(".js", ".webp")}</a></li>`
+    ).join('\n')}\n  </ul>`
+);
+await fs.writeFile(path.join('public', 'index.html'), output, 'utf8');
 
 // Generate HTML Files
 for(const script of scripts) {
